4e84bd0ed9de1ade9b52e7b943a328e2d31e1dda
Jalenarms1 <Jalenarms@outlook.com>
2025-02-04 15:02:29 -0800

    updating ui

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -11,29 +11,29 @@ import { MenuSheet } from './MenuSheet';
 const Hero = () => {
   return (
     <div className="flex flex-col snap-y snap-mandatory h-[100vh] overflow-y-scroll bg-neutral-700 relative font-mono">
-        <div className="p-5 flex justify-between items-center min-w-full border-b fixed top-0 z-[2] border-neutral-800 bg-black bg-opacity-50">
-            <div className="flex items-center gap-3">
-                <img className='shadow-md shadow-neutral-700 w-12 h-12 rounded-full hover:shadow-lg hover:shadow-neutral-600 cursor-pointer' src="https://firebasestorage.googleapis.com/v0/b/silly-socks-e0923.firebasestorage.app/o/anonymous-boy-icon-cartoon-style-vector.jpg?alt=media&token=e24c3ef2-6b93-42dc-aadd-c7b0a3ca2f82" />
-                
-                <div className="flex flex-col">
-                    <p className='text-xl text-white'>Jalen Arms</p>
-                    <a href='mailto:jalenarms@outlook.com' className='text-sm text-neutral-400'>jalenarms@outlook.com</a>
-                </div>
-            </div>
-
-            <div className="sm:flex gap-5 items-center hidden">
-                <button className='text-white text-sm'>About</button>
-                <button className='text-neutral-400 text-sm'>Showcase</button>
-                <button className='text-neutral-400 text-sm'>Contact</button>
-
-            </div>
-            
-            <MenuSheet />
-
-    
-    
-        </div>
         <section style={{backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/silly-socks-e0923.firebasestorage.app/o/premium_photo-1672940671025-113a634f83d1.jpeg?alt=media&token=dbc7b959-9026-4439-84ac-c3e7d9788a7a')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundColor: "black"}} className="section min-h-screen min-w-[100vw] bg-neutral-950 flex flex-col font-mono relative">
+            <div className="p-5 flex justify-between items-center min-w-full border-b  z-[2] border-neutral-900 bg-black">
+                <div className="flex items-center gap-3">
+                    <img className='shadow-md shadow-neutral-700 w-12 h-12 rounded-full hover:shadow-lg hover:shadow-neutral-600 cursor-pointer' src="https://firebasestorage.googleapis.com/v0/b/silly-socks-e0923.firebasestorage.app/o/anonymous-boy-icon-cartoon-style-vector.jpg?alt=media&token=e24c3ef2-6b93-42dc-aadd-c7b0a3ca2f82" />
+                    
+                    <div className="flex flex-col">
+                        <p className='text-xl text-white'>Jalen Arms</p>
+                        <a href='mailto:jalenarms@outlook.com' className='text-sm text-neutral-400'>jalenarms@outlook.com</a>
+                    </div>
+                </div>
+
+                <div className="sm:flex gap-5 items-center hidden">
+                    <button className='text-white text-sm'>About</button>
+                    <button className='text-neutral-400 text-sm'>Showcase</button>
+                    <button className='text-neutral-400 text-sm'>Contact</button>
+
+                </div>
+                
+                <MenuSheet />
+
+        
+        
+            </div>
             <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-t from-black/70 to-transparent"></div>
 
                 <div className="inset-0 flex flex-col absolute  bg-black bg-opacity-75">
