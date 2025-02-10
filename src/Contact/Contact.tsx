964f4ffa3357910e2561460b2697152a044abf14
Jalenarms1 <Jalenarms@outlook.com>
2025-02-10 15:17:45 -0800

    updating code

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,17 +1,64 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {motion, useInView} from "framer-motion"
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { BsPersonFill } from "react-icons/bs";
 
+interface ContactForm {
+    name: string,
+    email: string,
+    message: string
+}
 
 const Contact = ({onIsInView}: {onIsInView: () => void}) => {
+  const [contactForm, setContactForm] = useState<ContactForm>({} as ContactForm)
   const containerRef = useRef(null)
   const containerInView = useInView(containerRef, { once: false })
+  const [error, setError] = useState<string | null>(null)
 
   useEffect(() => {
       if (containerInView) { onIsInView() }
   }, [containerInView])
+
+  const handleContactFormUpdate = (e: React.ChangeEvent) => {
+    const {name, value} = e.target as HTMLInputElement
+
+    setContactForm({...contactForm, [name]: value})
+  }
+
+  function isValidEmail(email) {
+    const emailRegex = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
+
+  const handleSubmit = async () => {
+    if (contactForm.name.length < 1) { setError("Please enter a name.") }
+
+    if (contactForm.message.length < 1) { setError("Please enter a message.") }
+
+    if (!isValidEmail(contactForm.email)) { setError("Please enter a valid email.") }
+
+    try {
+        const resp = await fetch(import.meta.env.VITE_CONTACT_FORM_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": import.meta.env.VITE_PKEY_X1
+            },
+            body: JSON.stringify({
+                data: contactForm
+            })
+        })
+
+        const json = await resp.json()
+
+        console.log(json);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+  }
 
   return (
     <motion.div ref={containerRef} initial={{opacity: 0, y: 50}} animate={containerInView ? { opacity: 1, y: 0 } : {}} transition={{duration: 0.8, ease: "easeOut"}} id="Contact"  className="flex flex-col justify-start max-w-[100vw] lg:w-[75%] sm:w-[80%] mx-auto sm:gap-10 md:gap-10 gap-10 md:pt-20 md:p-0 p-5 md:h-[75vh] min-h-screen">
@@ -27,20 +74,20 @@ const Contact = ({onIsInView}: {onIsInView: () => void}) => {
                     <div className="flex items-center  md:gap-10 gap-2">
                         <div className="flex flex-col gap-2 w-full">
                             <p className='text-zinc-300'>Name</p>
-                            <Input type='text' className=' border-zinc-700 bg-zinc-200 text-black' />
+                            <Input name='name' value={contactForm.name} onChange={handleContactFormUpdate} type='text' className=' border-zinc-700 bg-zinc-200 text-black' />
                         </div>
                         <div className="flex flex-col gap-2 w-full">
                             <p className='text-zinc-300'>Email</p>
-                            <Input type='email' className=' border-zinc-700 bg-zinc-200' />
+                            <Input name='email' value={contactForm.email} onChange={handleContactFormUpdate} type='email' className='text-black  border-zinc-700 bg-zinc-200' />
                         </div>
 
                     </div>
                     <div className="flex flex-col gap-2 w-full">
                         <p className='text-zinc-300'>Message</p>
-                        <Textarea rows={50} className='w-full h-20 bg-zinc-200' />
+                        <Textarea value={contactForm.message} onChange={handleContactFormUpdate} name='message' rows={50} className='w-full h-20 bg-zinc-200 text-black' />
                     </div>
                     <div className="w-full flex justify-end ">
-                        <button className='px-6 py-2 bg-yellow-400 active:bg-yellow-500 text-black rounded-md'>Send</button>
+                        <button onClick={handleSubmit} className='px-6 py-2 bg-yellow-400 active:bg-yellow-500 text-black rounded-md'>Submit</button>
                     </div>
 
                 </div>
