f5e3b8a4453da952ea85773c274ea28d786754a9
Jalenarms1 <Jalenarms@outlook.com>
2025-02-10 21:20:28 -0800

    updating code

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -11,7 +11,7 @@ interface ContactForm {
 }
 
 const Contact = ({onIsInView}: {onIsInView: () => void}) => {
-  const [contactForm, setContactForm] = useState<ContactForm>({} as ContactForm)
+  const [contactForm, setContactForm] = useState<ContactForm>({name:"", email:"", message:""} as ContactForm)
   const containerRef = useRef(null)
   const containerInView = useInView(containerRef, { once: false })
   const [error, setError] = useState<string | null>(null)
@@ -33,11 +33,21 @@ const Contact = ({onIsInView}: {onIsInView: () => void}) => {
 
   const handleSubmit = async () => {
     setError(null)
-    if (contactForm.name.length < 1) setError("Please enter a name.")
+    if (contactForm?.name.length < 1) {
+        setError("Please enter your name.")
+        return
+    }
 
-    if (contactForm.message.length < 1) setError("Please enter a message.")
+    if (!isValidEmail(contactForm?.email)) {
+        setError("Please enter a valid email.")
+        return
+    } 
+    
+    if (contactForm?.message.length < 1) {
+        setError("Please enter a message.")
+        return
+    }
 
-    if (!isValidEmail(contactForm.email)) setError("Please enter a valid email.") 
 
     try {
         const resp = await fetch(import.meta.env.VITE_CONTACT_FORM_ENDPOINT, {
@@ -66,7 +76,7 @@ const Contact = ({onIsInView}: {onIsInView: () => void}) => {
   return (
     <motion.div ref={containerRef} initial={{opacity: 0, y: 50}} animate={containerInView ? { opacity: 1, y: 0 } : {}} transition={{duration: 0.8, ease: "easeOut"}} id="Contact"  className="flex flex-col justify-start max-w-[100vw] lg:w-[75%] sm:w-[80%] mx-auto sm:gap-10 md:gap-10 gap-10 md:pt-20 md:p-0 p-5 md:h-[75vh] min-h-screen">
         <div className="flex items-start gap-10 md:flex-row flex-col">
-            <div className="flex flex-col w-full md:w-[60%] gap-10">
+            <div className="flex flex-col w-full md:w-[60%] gap-5">
                 <div className="flex items-center justify-between">
                     <div className="flex flex-col gap-3">
                         <p className="text-3xl text-white font-semibold">Contact</p>
