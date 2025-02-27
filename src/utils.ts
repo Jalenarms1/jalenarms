4eecad8aa78789065310fe55e636bd6106e9312e
Jalen <105141749+Jalenarms1@users.noreply.github.com>
2025-02-27 16:58:09 -0700

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 export const downloadResume = async () => {
     const link = document.createElement("a");
-    link.href = "/src/assets/Jalen Arms - Resume 2025.pdf";
+    link.href = "/Jalen Arms - Resume 2025.pdf";
     link.download = "Jalen Arms - Resume 2025.pdf"; // Suggested filename
     document.body.appendChild(link);
     link.click();
